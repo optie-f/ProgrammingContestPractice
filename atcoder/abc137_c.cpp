83f0a403a27d1161a4d6c726cf5d0010bcfb54e8
optie <optie96@gmail.com>
2019-08-15 22:28:38 +0900

    init

---
+++ atcoder/abc137_c.cpp
@@ -0,0 +1,49 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP0(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define REP1(i,n) for(int i=1, i##_len=(n); i<=i##_len; ++i)
+#define RREP0(i,n) for(int i=n-1; i>=0; --i)
+#define RREP1(i,n) for(int i=n; i>=1; --i)
+
+#define SORT(c) sort((c).begin(),(c).end())
+#define SZ(a) int((a).size())
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define CLR(a) memset((a), 0 ,sizeof(a))
+
+inline int toInt(string s) {int v; istringstream sin(s);sin>>v;return v;}
+template<class T> inline string toString(T x) {ostringstream sout;sout<<x;return sout.str();}
+
+typedef long long LL;
+
+const int INTINF = 2147483647;
+const LL LLINF = 9223372036854775807;
+int gcd(int a,int b){return b?gcd(b,a%b):a;}
+
+void solve() {
+  LL result = 0;
+  int N;
+  cin >> N;
+  string s;
+  unordered_map<string, int> answermap;
+  REP0(i, N) {
+    cin >> s;
+    SORT(s);
+    if (EXIST(answermap, s)) {
+      result += answermap[s];
+      answermap[s]++;
+    } else {
+      answermap[s] = 1;
+    }
+  }
+  std::cout << result << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  solve();
+  return 0;
+}
