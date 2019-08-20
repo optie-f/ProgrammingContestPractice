3ccdd5cfa252b1af2c42873f83204931fde304f9
optie <optie96@gmail.com>
2019-08-20 21:37:54 +0900

    abc132-1_c

---
+++ atcoder/abc132_c.cpp
@@ -0,0 +1,39 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP0(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define REP1(i,n) for(int i=1, i##_len=(n); i<=i##_len; ++i)
+#define RREP0(i,n) for(int i=n-1; i>=0; --i)
+#define RREP1(i,n) for(int i=n; i>=1; --i)
+#define SORT(c) sort((c).begin(),(c).end())
+#define REVERSE(c) reverse((c).begin(),(c).end())
+#define FILL(c, v) fill((c).begin(),(c).end(), v)
+#define SZ(a) int((a).size())
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define CLR(a) memset((a), 0 ,sizeof(a))
+
+inline int toInt(string s) {int v; istringstream sin(s);sin>>v;return v;}
+template<class T> inline string toString(T x) {ostringstream sout;sout<<x;return sout.str();}
+
+typedef long long LL;
+
+const int INTINF = 2147483647;
+const LL LLINF = 9223372036854775807;
+int gcd(int a,int b){return b?gcd(b,a%b):a;}
+
+void solve() {
+  int N, tmp; cin >> N;
+  vector<int> d; d.reserve(N+1);
+  REP0(i, N) { cin >> tmp; d.push_back(tmp); }
+  SORT(d);
+  cout << d[N/2] - d[N/2-1] << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  solve();
+  return 0;
+}
