42c282ef40471297ff79b24944fe13608a28e06c
optie <optie96@gmail.com>
2020-01-28 19:41:23 +0900

    update libs, snippets

--- genTemplate.py
+++ genTemplate.py
@@ -5,24 +5,12 @@ using namespace std;
 #define FOR(i,a,b) for(int i=(a);i<(b);++i)
 #define REP0(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
 #define REP1(i,n) for(int i=1, i##_len=(n); i<=i##_len; ++i)
-#define RREP0(i,n) for(int i=n-1; i>=0; --i)
-#define RREP1(i,n) for(int i=n; i>=1; --i)
-#define SORT(c) sort((c).begin(),(c).end())
-#define REVERSE(c) reverse((c).begin(),(c).end())
-#define FILL(c, v) fill((c).begin(),(c).end(), v)
-#define SZ(a) int((a).size())
-#define EXIST(s,e) ((s).find(e)!=(s).end())
-#define CLR(a) memset((a), 0 ,sizeof(a))
-
-inline int toInt(string s) {int v; istringstream sin(s);sin>>v;return v;}
-template<class T> inline string toString(T x) {ostringstream sout;sout<<x;return sout.str();}
 
 typedef long long LL;
-typedef unsigned long long ULL;
+typedef pair<int, int> pii;
 
 const int INTINF = 1e9;
 const LL LLINF = 1e18;
-int gcd(int a,int b){return b?gcd(b,a%b):a;}
 
 void solve()
 {
--- libs/lib_string.cpp
+++ libs/lib_string.cpp
@@ -1,16 +1,35 @@
 #include <string>
+#include <sstream>
 using namespace std;
 
-string last (string s, int n)
+string last(string s, int n)
 {
-    string result=""s;
-    for(int i=0; i < n; i++) result = s[s.length() - i] + result;
+    string result = ""s;
+    for (int i = 0; i < n; i++)
+        result = s[s.length() - i] + result;
     return result;
 }
 
-string pad0 (string s, int n)
+string pad0(string s, int n)
 {
     string zeros = ""s;
-    for (int i=0; i < n; i++) zeros += "0";
+    for (int i = 0; i < n; i++)
+        zeros += "0";
     return last(zeros + s, n);
+}
+
+inline int toInt(string s)
+{
+    int v;
+    istringstream sin(s);
+    sin >> v;
+    return v;
+}
+
+template <class T>
+inline string toString(T x)
+{
+    ostringstream sout;
+    sout << x;
+    return sout.str();
 }
