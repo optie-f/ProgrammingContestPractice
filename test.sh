e90903548e2858032015fbd0e6d1e27fe1e84085
optie <optie96@gmail.com>
2020-01-30 18:18:11 +0900

    abc092_c

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 # from https://qiita.com/AokabiC/items/af685bfd205dda44ec45
 problemname=$1
 oj dl "https://atcoder.jp/contests/${problemname:0:6}/tasks/${problemname:0:8}"
-g++ -Wall -std=c++14 ./$1.cpp
+g++ -Wall -std=c++14 ./$1
 oj test
 rm -f a.out
 rm -rf test 
