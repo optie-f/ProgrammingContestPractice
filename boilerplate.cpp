a7b5de9c0e3c243e6d1c971bcebb8642e3097b02
optie <optie96@gmail.com>
2020-02-03 01:55:29 +0900

    abc055_c

--- boilerplate.cpp
+++ boilerplate.cpp
@@ -1,9 +1,9 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-#define FOR(i,a,b) for(int i=(a);i<(b);++i)
-#define REP0(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
-#define REP1(i,n) for(int i=1, i##_len=(n); i<=i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP0(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REP1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
 
 typedef long long LL;
 typedef pair<int, int> pii;
@@ -13,14 +13,13 @@ const LL LLINF = 1e18;
 
 void solve()
 {
-
 }
 
 int main(int argc, char const *argv[])
 {
     cin.tie(0);
     ios::sync_with_stdio(false);
-    std::cout<<std::fixed<<std::setprecision(10);
+    std::cout << std::fixed << std::setprecision(10);
     solve();
     return 0;
 }
