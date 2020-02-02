f2cb99c484e35bd64b55a5f1101012c6b3bec2b9
optie <optie96@gmail.com>
2020-02-02 21:05:26 +0900

    abc058_c, 環境整備

--- genTemplate.py
+++ genTemplate.py
@@ -1,10 +1,14 @@
-contest_name = input("contest name:")
-suffices = input("problem(s):")
-temp = './template.cpp'
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("contest_name")
+parser.add_argument("problems")
+args = parser.parse_args()
+
+temp = './boilerplate.cpp'
 
 with open(temp, mode='r') as tf:
     template = tf.read()
-    for suffix in suffices:
-        path = './{0}_{1}.cpp'.format(contest_name, suffix)
+    for suffix in args.problems:
+        path = './{0}_{1}.cpp'.format(args.contest_name, suffix)
         with open(path, mode='w') as f:
             f.write(template)
