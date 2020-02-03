4e6d995ccc8ea5e785d8321f453fe78738ac5d59
optie <optie96@gmail.com>
2020-02-03 14:36:19 +0900

    abc052_c

--- libs/number.cpp
+++ libs/number.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 int gcd(int a, int b)
 {
     return b ? gcd(b, a % b) : a;
@@ -16,4 +17,32 @@ long long gcd(long long a, long long b)
 long long lcm(long long a, long long b)
 { // bracket to avoid overflow
     return a * (b / gcd(a, b));
+}
+
+bool is_prime(long long N)
+{
+    if (N <= 1)
+        return false;
+    if (N == 2)
+        return true;
+    long long ub = std::sqrt(N);
+    if (ub * ub == N)
+        return false;
+    for (long long i = 3; i <= ub; i += 2)
+    {
+        if (N % i == 0)
+            return false;
+    }
+    return true;
+}
+
+long long prime_num(long long N, long long p)
+{
+    long long cnt = 0;
+    while (N % p == 0 and N > 1)
+    {
+        N /= p;
+        cnt++;
+    }
+    return cnt;
 }
