e0a6995867e2d5ab23a8cb3290b2040a1a016441
optie <optie96@gmail.com>
2020-02-06 14:39:36 +0900

    abc034

--- libs/number.cpp
+++ libs/number.cpp
@@ -1,13 +1,21 @@
 #include <cmath>
-int gcd(int a, int b)
-{
-    return b ? gcd(b, a % b) : a;
+
+long long modpow(long long a, long long b, long long mod)
+{// a^b % mod を log(b) で計算する
+    if (b == 0)
+        return 1;
+
+    if (b % 2 == 0)
+    {
+        long long d = modpow(a, b / 2, mod) % mod;
+        return (d * d) % mod;
+    }
+    else if (b % 2 == 1)
+    {
+        return (a * modpow(a, b - 1, mod)) % mod;
+    }
 }
 
-int lcm(int a, int b)
-{
-    return a * b / gcd(a, b);
-}
 
 long long gcd(long long a, long long b)
 {
