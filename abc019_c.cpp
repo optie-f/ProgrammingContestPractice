58259d9f4f0923a1f83757c4a7793e92cad00d9f
optie <optie96@gmail.com>
2020-02-11 20:11:37 +0900

    clang-format google

---
+++ abc019_c.cpp
@@ -0,0 +1,32 @@
+
+#include "bits/stdc++.h"
+using namespace std;
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP0(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REP1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+
+typedef long long LL;
+typedef pair<int, int> pii;
+
+const int INTINF = 1e9;
+const LL LLINF = 1e18;
+#define pow10(n) int(1e##n + n)
+
+void solve()
+{
+    int H; cin >> H;
+    int W; cin >> W;
+    int T; cin >> T;
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    std::cout << std::fixed << std::setprecision(10);
+    solve();
+    return 0;
+
+}
