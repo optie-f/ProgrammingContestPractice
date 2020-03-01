8f8fe3ea0fb0256e6254d4f4a7892ccddf9aa705
optie <optie96@gmail.com>
2020-03-02 00:36:09 +0900

    abc157

--- libs/union_find.cpp
+++ libs/union_find.cpp
@@ -1,22 +1,28 @@
 #include <vector>
 // インデックスで管理するものと思う
-class DisjointSets
+struct UnionFind
 {
-    std::vector<int> rank, p;
+    std::vector<int> rank, p, cnt;
 
-    DisjointSets(){};
-    DisjointSets(int size)
+    UnionFind(){};
+    UnionFind(int size)
+    {
+        init(size);
+    }
+
+    void init(int size)
     {
         rank.resize(size, 0);
         p.resize(size, 0);
-        for (int i = 0; i < size; i++)
-            makeSet(i);
+        cnt.resize(size, 0);
+        for (int i = 0; i < size; i++) makeSet(i);
     }
 
     void makeSet(int x)
     {
-        p[x] = x;
+        p[x]    = x;
         rank[x] = 0;
+        cnt[x]  = 1;
     }
 
     bool same(int x, int y)
@@ -26,6 +32,8 @@ class DisjointSets
 
     void unite(int x, int y)
     {
+        if (same(x, y))
+            return;
         link(findSet(x), findSet(y));
     }
 
@@ -34,10 +42,12 @@ class DisjointSets
         if (rank[x] > rank[y])
         {
             p[y] = x;
+            cnt[x] += cnt[y];
         }
         else
         {
             p[x] = y;
+            cnt[y] += cnt[x];
             rank[y] += (rank[x] == rank[y]);
         }
     }
@@ -49,5 +59,10 @@ class DisjointSets
             p[x] = findSet(p[x]);
         }
         return p[x];
+    }
+
+    int setSize(int x)
+    {
+        return cnt[findSet(x)];
     }
 };
