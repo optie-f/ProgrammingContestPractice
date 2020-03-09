163b43b50f734f4a705c6dee106c05fd88e13016
optie <optie96@gmail.com>
2020-03-09 17:20:07 +0900

    generalizing SegTree

--- libs/segment_tree.cpp
+++ libs/segment_tree.cpp
@@ -1,50 +1,46 @@
 #include <vector>
-// Range Min Query として実装
-// 二分木の各ノードに区間としての計算結果を持つ
-struct SegTree
+template <typename Monoid> struct SegTree
 {
-    // 一次元配列に完全二分木
-    std::vector<long long> data;
-    // 単位元 (min なら inf)
-    long long e = (1ll << 31) - 1;
+    typedef function<Monoid(Monoid, Monoid)> F;
+    std::vector<Monoid> data;  // 0-indexed 配列に完全二分木
+    const Monoid e;            // 単位元
+    const F f;
     int N = 1;
 
-    SegTree(int size)
+    SegTree(int size, const F f, const Monoid &e) : f(f), e(e)
     {
-        while (N < size)
-            N <<= 1;
-        // 要素数 n ではノード数は 2*n - 1
-        data.resize(2 * N - 1, e);
+        while (N < size) N <<= 1;
+        data.resize(2 * N - 1, e);  // 要素数 n => ノード数 2*n - 1
     }
 
-    void update(int i, long long x)
+    void update(int i, Monoid x)
     {
-        i += N - 1; // 要素 i の木中ノード番号
+        i += N - 1;  // 要素 i の木中ノード番号
         data[i] = x;
         while (i > 0)
         {
-            i = (i - 1) / 2; // 親ノード番号
-            data[i] = std::min(data[i * 2 + 1], data[i * 2 + 2]);
+            i       = (i - 1) / 2;  // 親ノード番号
+            data[i] = f(data[i * 2 + 1], data[i * 2 + 2]);
         }
     }
 
-    long long query(int q_l, int q_r)
-    { // [q_l, q_r) に対するクエリの処理
+    Monoid query(int q_l, int q_r)
+    {  // [q_l, q_r) に対するクエリの処理
         return _query(q_l, q_r, 0, 0, N);
     }
 
-private:
-    long long _query(int q_l, int q_r, int k, int k_l, int k_r)
-    { // ノード k : 区間は [k_l, k_r) について
+   private:
+    Monoid _query(int q_l, int q_r, int k, int k_l, int k_r)
+    {  // ノード k : 区間は [k_l, k_r) について
         if (k_r <= q_l || q_r <= k_l)
-            return e; // 交差しない
+            return e;  // 交差しない
         if (q_l <= k_l && k_r <= q_r)
-            return data[k]; // 完全に含む→受け止める
+            return data[k];  // 完全に含む→受け止める
         else
         {
-            long long vl = _query(q_l, q_r, k * 2 + 1, k_l, (k_l + k_r) / 2);
-            long long vr = _query(q_l, q_r, k * 2 + 2, (k_l + k_r) / 2, k_r);
-            return std::min(vl, vr);
+            Monoid vl = _query(q_l, q_r, k * 2 + 1, k_l, (k_l + k_r) / 2);
+            Monoid vr = _query(q_l, q_r, k * 2 + 2, (k_l + k_r) / 2, k_r);
+            return f(vl, vr);
         }
     }
 };
